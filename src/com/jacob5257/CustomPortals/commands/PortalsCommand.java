c87d192a685781cc139ce6361386b3153efedfe5
Jacob <lijacob290@gmail.com>
2024-02-18 21:39:07 -0700

    small typo

--- src/com/jacob5257/CustomPortals/commands/PortalsCommand.java
+++ src/com/jacob5257/CustomPortals/commands/PortalsCommand.java
@@ -22,7 +22,7 @@ public class PortalsCommand implements CommandExecutor {
                 commandSender.sendMessage("§6/portals §7- §aDisplays plugin information.");
                 commandSender.sendMessage("§6/portals help §7- §aDisplays this help message.");
                 if (commandSender.hasPermission("customportals.reset")) {
-                    commandSender.sendMessage("§6/portals config §7- §Dump the configuration file to chat for debugging.");
+                    commandSender.sendMessage("§6/portals config §7- §aDump the configuration file to chat for debugging.");
                     commandSender.sendMessage("§6/portals list §7- §aLists all portals.");
                     commandSender.sendMessage("§6/portals reset §7- §aResets all portals. Use with caution!");
                     commandSender.sendMessage("§6/portals reload §7- §aReloads the plugin and all portals.");
